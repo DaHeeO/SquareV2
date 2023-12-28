be131119e41a56292e46a98561c734a4a1530026
연제경 <yllh325@gmail.com>
2023-12-28 22:47:44 +0900

    폴더구조

--- App.tsx
+++ App.tsx
@@ -2,12 +2,16 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import Login from './screens/login/Login';
+import { RecoilRoot } from 'recoil';
+
+import Login from './src/screens/login/Login';
 
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
+
+    <RecoilRoot>
     <NavigationContainer>
       <Stack.Navigator>
         {/* 로그인 스크린을 첫 번째 화면으로 설정합니다. */}
@@ -15,6 +19,7 @@ const App = () => {
         {/* 다른 화면들을 여기에 추가합니다. */}
       </Stack.Navigator>
     </NavigationContainer>
+    </RecoilRoot>
   );
 };
 
