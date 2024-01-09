1253a31a51ea29320f030a1a27bf085eb9c7e2c9
오다희 <odahui@odahuis-MacBook-Pro.local>
2024-01-09 20:10:29 +0900

    feat: permission page and Font settings

---
+++ src/screens/login/Permission.styles.ts
@@ -0,0 +1,33 @@
+import styled from "styled-components/native";
+import {colors} from '../../components/common/globalStyles'
+
+export const Container = styled.View`
+  flex: 1;
+  justify-content: flex-start;
+  align-items: flex-start;
+`;
+
+export const Top = styled.View`
+  align-items: center;
+  padding: 10px 0px;
+  width: 100%;
+`;
+
+export const Main = styled.View`
+  padding: 10px 0px;
+  margin: 0px 24px;
+`;
+
+export const Notification = styled.View`
+  flex-direction: row;
+  align-items: center;
+  margin: 20px 0px;
+`
+
+export const Div = styled.View`
+  margin-left: 12px;
+`
+
+export const TextForm = styled.View`
+  flex-direction: row;
+`
